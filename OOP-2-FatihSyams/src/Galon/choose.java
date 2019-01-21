cff2640997d9cb10a343227931d72a24ef3d45e1
fatihsyams <43774677+fatihsyams@users.noreply.github.com>
2019-01-21 09:03:00 +0700

    Add files via upload

---
+++ OOP-2-FatihSyams/src/Galon/choose.java
@@ -0,0 +1,46 @@
+package Galon;
+
+public class choose {
+
+	public static int ttgl = 07;
+	 public static int bbln = 11;
+	 public static int ttahun = 2019;
+	 
+	public static int waktu (int ttgl, Integer bbln, Integer ttahun) {
+	      System.out.println(" ttl,  bbln, ttahun ");
+		return(waktu);
+	}
+
+	
+	 static String x = "Have Nice A Day";
+	
+	public static void main(String args[]) {
+		
+		// Jika Milih A Maka Keluar SUSU
+		// JIka Milih B Maka Keluar KOPI
+		galoncanggih.main(args);
+		
+	      switch('B') {
+	         case 'A' :
+	            System.out.println("susu anda telah siapp, sihlakan di nikmati");
+	            break;
+	         case 'B' :
+	            System.out.println("Kopi anda telah siap, sihlakan di nikmati!");
+	            break;
+	      }
+	      System.out.println(x);
+	      
+     	for (int itung = 1; itung < 4; itung = 1 + itung) {
+			System.out.println(itung);
+		}
+     	
+     	int x = 3;
+     	int y = 2;
+		System.out.println((x > y)? "Sedang di isi Galonnya" : "Galonnya masih FULL");
+		System.out.println(( x > y) +"              Air Sudah Bisa di Gunakan Kembali..");
+
+     		
+     	
+	   }
+	
+}
---
+++ OOP-2-FatihSyams/src/Galon/galoncanggih.java
@@ -0,0 +1,23 @@
+package Galon;
+
+public class galoncanggih {
+	
+
+	public static void main (String[] args) {
+		
+		int x = 5;
+		do {
+			System.out.println(x);
+			x--;
+		}while (x > 0 );
+	
+		
+		int MengisiAir = 5;
+		
+		if (MengisiAir == 5) {
+			System.out.println("Gelas Anda Sudah Penuh..");
+		}else {
+			System.out.println("Gawattt Gelas Anda ISInya Ngeluberrr.. Cepat di AMBIL!!!");
+		} 
+	}
+}
